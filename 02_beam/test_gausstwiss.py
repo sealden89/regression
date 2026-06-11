7240f9d0e90416342d45a256571b1a3785b4f947
Siobhan Alden <siobhanalden@Siobhans-MacBook-Pro.local>
2026-06-11 15:09:04 +0100

    gausstwiss test functioning to compare beam size and emittance to input values.

--- 02_beam/test_gausstwiss.py
+++ 02_beam/test_gausstwiss.py
@@ -14,7 +14,7 @@ def test() :
     optics_name   = base_name+"_optics.root"
 
     l  = 2.0 
-    data = {
+    params = {
         'BETX': '4',
         'BETY' : '4',
         'EMITX' : '5e-7',
@@ -22,11 +22,11 @@ def test() :
     }
     ngenerate = 5000
 
-    pybdsim.Run.RenderGmadJinjaTemplate(template_name,gmad_name,data)
+    pybdsim.Run.RenderGmadJinjaTemplate(template_name,gmad_name,params)
     pybdsim.Run.Bdsim(gmad_name,base_name,ngenerate,1)
 
-    data = pybdsim.DataPandas.BDSIMOutput("root_name.root")
-    primary_data = d.get_primary()
+    data = pybdsim.DataPandas.BDSIMOutput(root_name)
+    primary_data = data.get_primary()
     
     primary_x = primary_data['x']
     primary_xp = primary_data['xp']
@@ -34,34 +34,36 @@ def test() :
     primary_yp = primary_data['yp']  
     primary_t = primary_data['T']
     primary_energy = primary_data['energy']
-    n=len(x)
+    number_particles=len(primary_x)
 
-    cov_xxp = np.cov(x,xp)
-    cov_yyp = np.cov(y,yp)
-    cov_et = np.cov(y,yp)
+    cov_xxp = np.cov(primary_x,primary_xp)
+    cov_yyp = np.cov(primary_y,primary_yp)
 
 
     sigma_x_generated = np.sqrt(cov_xxp[0][0])
     sigma_xp_generated = np.sqrt(cov_xxp[1][1])
     sigma_y_generated = np.sqrt(cov_yyp[0][0])
     sigma_yp_generated = np.sqrt(cov_yyp[1][1])
-    sigma_x_calculated = np.sqrt(float(data['BETX'])*float(data['EMITX']))
-    sigma_y_calculated = np.sqrt(float(data['BETY'])*float(data['EMITY']))
+    sigma_x_calculated = np.sqrt(float(params['BETX'])*float(params['EMITX']))
+    sigma_y_calculated = np.sqrt(float(params['BETY'])*float(params['EMITY']))
 
     
     emittance_x_generated = np.sqrt(cov_xxp[0][0]*cov_xxp[1][1]-cov_xxp[0][1]*cov_xxp[1][0])
     emittance_y_generated = np.sqrt(cov_yyp[0][0]*cov_yyp[1][1]-cov_yyp[0][1]*cov_yyp[1][0])
-    emittance_x_input = float(data['EMITX'])
-    emittance_y_input = float(data['EMITY'])
+    emittance_x_input = float(params['EMITX'])
+    emittance_y_input = float(params['EMITY'])
 
 
     print("assert sigma_x_calculated = ",sigma_x_calculated, "= sigma_x_generated = ", sigma_x_generated)
     print("assert sigma_y_calculated = ",sigma_y_calculated, "= sigma_y_generated = ", sigma_y_generated)
     print("assert emittance_x_input = ", emittance_x_input, "= emittance_x_calculated = ", emittance_x_generated)
     print("assert emittance_y_input = ", emittance_y_input, "= emittance_y_calculated = ", emittance_y_generated)
+    assert(number_particles == ngenerate)
+    assert(sigma_x_calculated == pytest.approx(sigma_x_generated,abs=1e-3))
+    assert(sigma_y_calculated == pytest.approx(sigma_x_generated,abs=1e-3))
+    assert(emittance_x_input == pytest.approx(emittance_x_generated,abs=1e-3))
+    assert(emittance_y_input == pytest.approx(emittance_y_generated,abs=1e-3))
 
-
-    #assert(n = ngenerate)
 
 
 
